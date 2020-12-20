461576b14d7abec154dbf5a6ad71a6396cfab92a
Sanni Nurmela <sanni.nurmela@gmail.com>
2020-12-20 13:06:30 +0200

    Added palindrometest.js file

---
+++ palindrometest.js
@@ -0,0 +1,31 @@
+﻿const readlineSync = require('readline-Sync');
+const minLength = 3;
+console.log('***Palindrome-generator***');
+
+let input;
+let i = 0;
+let isPalindrome = true;
+
+do {
+  input = readlineSync.question('Enter the input: ');
+
+  if (input.length < minLength) {
+    console.log(`Your input is shorter than ${minLength} characters  `);
+  }
+} while (input.length < minLength);
+
+const word = input.replace(/\s+/g, '').toUpperCase();
+
+const normal = [...word];
+const flipped = [...normal].reverse();
+
+while (i < normal.length) {
+  if (normal[i] !== flipped[i]) {
+    isPalindrome = false;
+  }
+  i++;
+}
+
+console.log(
+  ` Your input "${input}" ${isPalindrome ? 'is a' : 'is not'} palindrome `
+);
